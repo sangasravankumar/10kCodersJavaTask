b5afed68d78b3223b763d0a5442fc2d2f0bfeeee
sangasravankumar <147960967+sangasravankumar@users.noreply.github.com>
2025-05-25 22:37:22 +0530

    Add files via upload

---
+++ Datatypes/Arthmeticoperators.java
@@ -0,0 +1,18 @@
+public class Arthmeticoperators
+{
+public static void main (String [] args)
+{
+int a=10;
+int b=2;
+int c=a+b;
+System.out.println("Addition of a and b is "+ c );
+int s=a-b;
+System.out.println("Subtraction of a and b is "+ s );
+int m=a*b;
+System.out.println("Multiplication of a and b is "+ m );
+int d=a/b;
+System.out.println("Division of a and b is "+ d );
+int mod=a%b;
+System.out.println("Modulus of a and b is "+ mod );
+}
+}
---
+++ Datatypes/datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes
+{
+public static void main (String []args)
+{
+byte b=1;
+System.out.println(b);
+short s=10;
+System.out.println(s);
+int i=5;
+System.out.println(i);
+long l=1234568878;
+System.out.println(l);
+float f=12.45f;
+System.out.println(f);
+double d=12202.2d;
+System.out.println(d);
+char c= 'A';
+System.out.println(c);
+Boolean bl=true;
+System.out.println(bl);
+}
+}
